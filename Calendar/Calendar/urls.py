ae78616cbfd73cf67b2ec2885e3ef3114ef66518
young <youngcher1@naver.com>
2023-07-10 16:51:28 +0900

    done

--- Calendar/Calendar/urls.py
+++ Calendar/Calendar/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('calendar/', include('calendarapp.urls')),
+    path('', include('calendarapp.urls')),
 ]
